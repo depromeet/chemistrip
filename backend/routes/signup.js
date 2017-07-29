166ddef94be3030164b79cb19381509245aac99a
root <saven7788@gmail.com>
2017-07-29 20:47:40 +0900

    change code style

--- backend/routes/signup.js
+++ backend/routes/signup.js
@@ -3,8 +3,8 @@ var router = express.Router();
 var app = express();
 
 
-// router.route('/').post(function(req,res){});
-router.post('/', function(req, res, next) {
+router.route('/').post(function(req,res){
+//router.post('/', function(req, res, next) {
 
     console.log("time" + Date.now());
     res.json('depromeet');
