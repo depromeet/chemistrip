a1d83dc6d0ec528ccbc1e56b38c140dcf216ecc7
root <saven7788@gmail.com>
2017-07-30 16:08:44 +0900

    post /signup complete except error handling

--- backend/routes/signup.js
+++ backend/routes/signup.js
@@ -3,11 +3,57 @@ var router = express.Router();
 var app = express();
 
 
-router.route('/').post(function(req,res){
-//router.post('/', function(req, res, next) {
+var admin = require("firebase-admin");
 
-    console.log("time" + Date.now());
-    res.json('depromeet');
+var serviceAccount = require('../config/firebase_config.json');
+
+admin.initializeApp({
+	credential: admin.credential.cert(serviceAccount),
 });
 
+router.route('/').post(function(req,res){
+
+	let tokenArr = [];
+
+
+	const uid = req.body.uid;
+
+	admin.auth().createCustomToken(uid)
+	.then(function(customToken) {
+
+		tokenArr.push(customToken);	  
+
+		
+		
+		res.json({
+		result: true,
+		msg: "토큰이 발급되었습니다.",
+		data : tokenArr
+	});	
+
+		
+		console.log("#",customToken);
+	})
+  	.catch(function(error) {
+		      console.log("Error creating custom token:", error);
+			    });
+
+	
+});
+
+
+
+/*
+request -
+{
+	    "uid": "124125125124123123",
+			    "name": "jang",
+				    "email": "test@email.com"
+}
+
+response -
+{
+	    "token": "adsfasdfafasfds"
+}
+*/
 module.exports = router;
