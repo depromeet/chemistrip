e874679aeefd72bb7e58f4a39c8f75a2c30cb7fb
joowon <saven7788@gmail.com>
2017-07-30 16:20:23 +0900

    complete error handling with Oatuh

--- backend/routes/signup.js
+++ backend/routes/signup.js
@@ -13,32 +13,65 @@ admin.initializeApp({
 
 router.route('/').post(function(req,res){
 
-	let tokenArr = [];
-
+	if( !req.body.uid ){
+        res.json({
+            result: false,
+            msg: "req.body.uid이 없습니다."
+        });
+        return;
+    }else if( !req.body.name ){
+        res.json({
+            result: false,
+            msg: "req.body.name이 없습니다."
+        });
+        return;
+    }else if( !req.body.email ){
+        res.json({
+            result: false,
+            msg: "req.body.email이 없습니다."
+        });
+        return;
+    }
 
 	const uid = req.body.uid;
-
 	admin.auth().createCustomToken(uid)
 	.then(function(customToken) {
-
-		tokenArr.push(customToken);	  
-
-		
-		
 		res.json({
-		result: true,
-		msg: "토큰이 발급되었습니다.",
-		data : tokenArr
-	});	
+			result: true,
+			msg: "토큰이 발급되었습니다.",
+			data : customToken
+		});
 
-		
-		console.log("#",customToken);
 	})
   	.catch(function(error) {
-		      console.log("Error creating custom token:", error);
-			    });
-
-	
+		console.log(error+Date.now());
+		res.json({
+			result: false,
+			msg: "토큰이 발급을 실패했습니다.",
+			data: error
+		});
+	});
+	//TODO insert문 아직 안넣었다.
+	// pool.query( 'update duckmate.member set firebasToken = ? or today_alarm = ? where member_id = ?;', [ req.body.firebasetoken, req.body.today_alarm,req.body.member_id] , function( err, results ) {
+    //     if (err){
+    //         res.json({
+    //             result: false,
+    //             msg: "db 접속 에러"
+    //         });
+    //         return;
+    //     }
+    //     if( results.affectedRows === 1 ){
+    //         res.status(201).json({
+    //             result: true,
+    //             msg: "업데이트가 완료되었습니다.",
+    //         });
+    //     }else{
+    //         res.status(201).json({
+    //             result: false,
+    //             msg: "업데이트를 실패했습니다.",
+    //         });
+    //     }
+    // });
 });
 
 
