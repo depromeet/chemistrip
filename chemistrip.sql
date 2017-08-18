1a865a4b8fcf3b68fe364d2cda9f2d59ec94efc3
joowon <saven7788@gmail.com>
2017-08-18 21:41:04 +0900

    sql업데이

--- chemistrip.sql
+++ chemistrip.sql
@@ -1,26 +1,24 @@
-CREATE TABLE `trip` (
-  `trip_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'usr_id',
-  `firebase_token` text NOT NULL COMMENT 'firebase_token',
-  `city` varchar(45) DEFAULT NULL COMMENT '여행 도시',
-  `date` varchar(45) DEFAULT NULL COMMENT '여행 기간',
-  `member_count` int(11) DEFAULT NULL COMMENT '원하는 동행 수',
-  `member_sex` tinyint(4) DEFAULT NULL COMMENT '동행의 성별',
-  PRIMARY KEY (`trip_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
 CREATE TABLE `usr` (
   `name` varchar(45) CHARACTER SET utf8 DEFAULT '' COMMENT '회원 닉네임',
-  `img` varchar(225) CHARACTER SET utf8 DEFAULT '' COMMENT '회원 사진',
-  `sex` tinyint(11) DEFAULT NULL COMMENT '회원 성별',
+  `gender` varchar(11) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '회원 성별',
+  `birthDate` int(11) DEFAULT NULL COMMENT '회원 생일',
   `age` int(11) DEFAULT NULL COMMENT '회원 나이',
   `email` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '회원 메일주소',
   `passwd` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '회원 비밀번호',
   `kind_sns` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'sns 종류',
-  `firebase_token` longtext COLLATE utf8_unicode_ci NOT NULL COMMENT 'sns token',
-  `firebase_session` varchar(225) COLLATE utf8_unicode_ci DEFAULT '' COMMENT 'sns token',
-  `date` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '여행기간',
-  `city` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '여행 도시',
-  `member_count` int(11) DEFAULT NULL COMMENT '원하는 동행 수',
-  `firebase_uid` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `firebaseToken` longtext COLLATE utf8_unicode_ci NOT NULL COMMENT 'sns token',
+  `firebaseSession` varchar(225) COLLATE utf8_unicode_ci DEFAULT '' COMMENT 'sns token',
+  `schedule` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '여행기간',
+  `memberCount` int(11) DEFAULT NULL COMMENT '원하는 동행 수',
+  `uid` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'firebaseuid',
+  `answer` json DEFAULT NULL,
+  `id` int(100) unsigned NOT NULL AUTO_INCREMENT COMMENT '고유 id [서버로 쏴줄 때 uid랑 firebasetoken으로 primarly key 써서 주면 문제가 됨으로]',
+  `destination` int(11) DEFAULT NULL COMMENT '여행 도시',
+  `country` int(11) DEFAULT NULL COMMENT '본인 국적',
+  `minDate` int(11) DEFAULT NULL COMMENT '여행 첫날',
+  `maxDate` int(11) DEFAULT NULL COMMENT '여행 마지막날',
+  `minAge` int(11) DEFAULT NULL COMMENT '원하는 최소 나이',
+  `maxAge` int(11) DEFAULT NULL COMMENT '원하는 최대 나이',
+  `preferenceGender` int(11) DEFAULT NULL COMMENT '선호 성별',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
