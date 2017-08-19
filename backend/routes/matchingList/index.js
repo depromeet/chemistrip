af74914267d0219ee83c73c5112e863b88a2ad12
joowon <saven7788@gmail.com>
2017-08-19 11:18:42 +0900

    #

--- backend/routes/matchingList/index.js
+++ backend/routes/matchingList/index.js
@@ -15,7 +15,7 @@ router.route('/').get((req,res) => {
 		}
         if( rows.length === 1 ){
             mainAnswer = rows[0].answer;
-            console.log("mainAnswer",mainAnswer);
+            CheckOthersAnswer( mainAnswer );
 		}else{
             res.status(201).json({
 				result: true,
@@ -25,38 +25,41 @@ router.route('/').get((req,res) => {
         }
     });
 
-    pool.query( 'select id,name,minDate,maxDate,gender,answer from chemistrip.usr' , function(err, rows) {
-		if (err){
-			res.json({
-				result: false,
-				msg: "db 접속 에러",
-				qry: this.sql
-			});
-			return;
-		}
-        console.log("rows's value",rows);
+    const CheckOthersAnswer = ( mainAnswer ) =>{
+        pool.query( 'select id,name,minDate,maxDate,gender,answer from chemistrip.usr' , function(err, rows) {
+    		if (err){
+    			res.json({
+    				result: false,
+    				msg: "db 접속 에러",
+    				qry: this.sql
+    			});
+    			return;
+    		}
+            console.log("before transforming rows's value",rows);
+            try{ rows = JSON.parse( JSON.stringify(rows) ); }
+            catch(e){ console.log("JSON.stringify, JSON.parse error ",e); }
+            console.log("after transforming rows's value",rows);
 
-        try{
-            rows = JSON.parse( JSON.stringify(rows) );
-        }catch(e){
-            console.log("JSON.stringify, JSON.parse error ",e);
-        }
+            console.log(mainAnswer[0],mainAnswer[1],mainAnswer[2]);
+            console.log("rows[0].answer[0]은?",rows[0].answer[0]);
+            res.send(rows);
+    		// if( rows.length === 1 ){
+    		// 	res.status(201).json({
+    		// 		result: true,
+    		// 		message: "회원정보 입력이 완료되었습니다.",
+    		// 	});
+    		// 	return;
+    		// }else{
+            //     res.status(201).json({
+    		// 		result: false,
+    		// 		message: "회원정보가 잘못입력되었습니다.",
+    		// 	});
+    		// 	return;
+            // }
+    	});
+    };
 
-        res.send(rows);
-		// if( rows.length === 1 ){
-		// 	res.status(201).json({
-		// 		result: true,
-		// 		message: "회원정보 입력이 완료되었습니다.",
-		// 	});
-		// 	return;
-		// }else{
-        //     res.status(201).json({
-		// 		result: false,
-		// 		message: "회원정보가 잘못입력되었습니다.",
-		// 	});
-		// 	return;
-        // }
-	});
+
 });
 
 module.exports = router;
